300560c9bcd4e2ece5c9bf5b5d7a0dc99db5ad2f
QuietClickCode <2572417548@qq.com>
2020-01-20 00:53:22 +0800

    新增测试页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,6 +15,11 @@ Page({
       url: '../logs/logs'
     })
   },
+  toTest: function() {
+    wx.navigateTo({
+      url: '../test/test'
+    })
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
