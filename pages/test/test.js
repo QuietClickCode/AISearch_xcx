726db9a14bbc01a291e58d5a897631173ce1b97c
QuietClickCode <2572417548@qq.com>
2020-01-23 03:44:57 +0800

    2020年1月23日03:43:28
    搜索界面出来了,flex布局需要好好学一下

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,18 +1,28 @@
 // pages/test/test.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    test: "",
   },
-
+  show: function () {
+    this.setData({
+      test: "测试页面"
+    })
+  },
+  search: function () {
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.show();
   },
 
   /**
